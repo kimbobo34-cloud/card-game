d7bf051cf79e9b6704ff2468632cf412ef9200fa
kimbobo34-cloud <kimbobo34@gmail.com>
2025-09-29 23:10:59 +0900

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let countdown;
 let timeLeft = totalTime;
 
 const images = [];
-for (let i = 1; i <= 10; i++) {
+for (let i = 1; i <= 8; i++) {
   images.push(`img/${i}.jpg`);
   images.push(`img/${i}.jpg`);
 }
